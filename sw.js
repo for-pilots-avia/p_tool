5d5c048a3d3bb9c9969e2a3347ebb42b209c8318
Denis-Tikhonov <abrikos2384@gmail.com>
2026-06-11 16:15:46 +0500

    sw 30

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-/* Pilot's Tool — Service Worker v24 */
+/* Pilot's Tool — Service Worker v30 */
 
-var CACHE_NAME = 'pilots-tool-v29';
+var CACHE_NAME = 'pilots-tool-v30';
 
 var JSON_MODULE_NAMES = {
   'modules/metbriefing/data/metbriefing.json':            'Метео брифинг',
