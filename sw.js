a2c3b3730e069df00f57046c90d83c2e51f6b557
Denis-Tikhonov <abrikos2384@gmail.com>
2026-06-18 23:39:22 +0500

    fonts
    sw 41

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-/* Pilot's Tool — Service Worker v40 */
+/* Pilot's Tool — Service Worker v41 */
 
-var CACHE_NAME = 'pilots-tool-v40';
+var CACHE_NAME = 'pilots-tool-v41';
 
 var JSON_MODULE_NAMES = { 
   'modules/aviation_sayings.json':                        'Авиационные цитаты',
