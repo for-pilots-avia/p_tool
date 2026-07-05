0fe30a7cfb2d1dbd701c92d6b4211e35e1636397
Denis-Tikhonov <abrikos2384@gmail.com>
2026-07-05 14:18:15 +0500

    css
    для iPad
    sw 47

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-/* Pilot's Tool — Service Worker v46 */
+/* Pilot's Tool — Service Worker v47 */
 
-var CACHE_NAME = 'pilots-tool-v46';
+var CACHE_NAME = 'pilots-tool-v47';
 
 var JSON_MODULE_NAMES = {
   'modules/aviation_sayings.json':                        'Авиационные цитаты',
